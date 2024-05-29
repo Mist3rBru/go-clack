21cc17b39c12af9574c3de91a19b13c553dee383
Bruno Rocha <brunorocha2674@gmail.com>
2024-05-29 17:22:59 -0300

    test: add base prompt tests

---
+++ internal/prompt/prompt_test.go
@@ -0,0 +1,111 @@
+package prompt_test
+
+import (
+	"go-clack/internal/prompt"
+	"math/rand"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEmitEvent(t *testing.T) {
+	p := prompt.DefaultPrompt(true)
+	event := "test"
+	arg := rand.Int()
+
+	p.On(event, func(args ...any) {
+		assert.Equal(t, args[0], arg)
+	})
+	p.Emit(event, arg)
+}
+
+func TestEmitOtherEvent(t *testing.T) {
+	p := prompt.DefaultPrompt(true)
+	event := "test"
+	calledTimes := 0
+
+	p.On(event, func(args ...any) {
+		calledTimes++
+	})
+	p.Emit("other" + event)
+	assert.Equal(t, 0, calledTimes)
+}
+
+func TestEmitEventWithMultiArgs(t *testing.T) {
+	p := prompt.DefaultPrompt(true)
+	event := "test"
+	args := []any{rand.Int(), rand.Int()}
+
+	p.On(event, func(_args ...any) {
+		assert.Equal(t, _args, args)
+	})
+	p.Emit(event, args...)
+	p.Emit(event, args[0], args[1])
+}
+
+func TestEmitEventTwice(t *testing.T) {
+	p := prompt.DefaultPrompt(true)
+	event := "test"
+	calledTimes := 0
+
+	p.On(event, func(args ...any) {
+		calledTimes++
+	})
+	p.Emit(event)
+	p.Emit(event)
+	assert.Equal(t, 2, calledTimes)
+}
+
+func TestEmitEventOnce(t *testing.T) {
+	p := prompt.DefaultPrompt(true)
+	event := "test"
+	calledTimes := 0
+
+	p.Once(event, func(args ...any) {
+		calledTimes++
+	})
+	p.Emit(event)
+	p.Emit(event)
+	assert.Equal(t, 1, calledTimes)
+}
+
+func TestEmitUnsubscribedEvent(t *testing.T) {
+	p := prompt.DefaultPrompt(true)
+	event := "test"
+	calledTimes := 0
+	listener := func(args ...any) {
+		calledTimes++
+	}
+
+	p.On(event, listener)
+	p.Off(event, listener)
+	p.Emit(event)
+	assert.Equal(t, 0, calledTimes)
+}
+
+func TestSetValue(t *testing.T) {
+	p := prompt.DefaultPrompt(true)
+	p.Value = ""
+	p.SetValue("test")
+	assert.Equal(t, "test", p.Value)
+}
+
+func TestPaseKey(t *testing.T) {
+	p := prompt.DefaultPrompt(true)
+
+	key, char := p.ParseKey('\n', []byte{})
+	assert.Equal(t, "Enter", key)
+	assert.Equal(t, "\n", char)
+
+	key, char = p.ParseKey('a', []byte{})
+	assert.Equal(t, "a", key)
+	assert.Equal(t, "a", char)
+
+	key, char = p.ParseKey(3, []byte{})
+	assert.Equal(t, "Cancel", key)
+	assert.Equal(t, "", char)
+
+	key, char = p.ParseKey(27, []byte{'[', 'A'})
+	assert.Equal(t, "ArrowUp", key)
+	assert.Equal(t, "", char)
+}
