9a5b0712c1bf209ced0cff6539412632926a3759
Bruno Rocha <brunorocha2674@gmail.com>
2024-08-07 15:16:16 -0300

    feat(prompts): add more methods to Workflow

--- prompts/workflow.go
+++ prompts/workflow.go
@@ -6,48 +6,126 @@ import (
 	"strings"
 )
 
-type workflowStep struct {
-	Name   string
-	Prompt func() (any, error)
+type WorkflowStep struct {
+	Name      string
+	Prompt    func() (any, error)
+	Condition func() bool
+	SetResult bool
 }
 
-type workflowBuilder struct {
-	steps  []*workflowStep
-	result any
+type WorkflowBuilder struct {
+	steps    []*WorkflowStep
+	onCancel func(step string, err error)
+	result   any
 }
 
-func (w *workflowBuilder) Step(name string, prompt func() (any, error)) *workflowBuilder {
-	w.steps = append(w.steps, &workflowStep{Name: name, Prompt: prompt})
+// Step adds a new step to the workflow with a prompt function to gather the step's result.
+func (w *WorkflowBuilder) Step(name string, prompt func() (any, error)) *WorkflowBuilder {
+	w.steps = append(w.steps, &WorkflowStep{
+		Name: name, Prompt: prompt,
+		SetResult: true,
+	})
 	return w
 }
 
-func (w *workflowBuilder) Run() error {
+// ConditionalStep adds a new step that runs only if the provided condition is true.
+func (w *WorkflowBuilder) ConditionalStep(name string, condition func() bool, prompt func() (any, error)) *WorkflowBuilder {
+	w.steps = append(w.steps, &WorkflowStep{
+		Name:      name,
+		Condition: condition,
+		Prompt:    prompt,
+		SetResult: true,
+	})
+	return w
+}
+
+// ForkStep adds a new step that runs a sub-workflow if the provided condition is true.
+func (w *WorkflowBuilder) ForkStep(name string, condition func() bool, subWorkflow func() *WorkflowBuilder) *WorkflowBuilder {
+	w.steps = append(w.steps, &WorkflowStep{
+		Name:      name,
+		Condition: condition,
+		Prompt: func() (any, error) {
+			err := subWorkflow().Run()
+			return nil, err
+		},
+		SetResult: false,
+	})
+	return w
+}
+
+// LogStep adds a step that performs an action without affecting the workflow's result.
+// Typically used for logging or other side effects.
+func (w *WorkflowBuilder) LogStep(name string, action func()) *WorkflowBuilder {
+	w.steps = append(w.steps, &WorkflowStep{
+		Name: name,
+		Prompt: func() (any, error) {
+			action()
+			return nil, nil
+		},
+		SetResult: false,
+	})
+	return w
+}
+
+// CustomStep adds a custom step to the workflow. The custom step can define its own behavior.
+func (w *WorkflowBuilder) CustomStep(step *WorkflowStep) *WorkflowBuilder {
+	w.steps = append(w.steps, step)
+	return w
+}
+
+// OnCancel sets a callback function to be called if any step encounters an error and the workflow is onCanceled.
+func (w *WorkflowBuilder) OnCancel(onCancel func(step string, err error)) *WorkflowBuilder {
+	w.onCancel = onCancel
+	return w
+}
+
+// Run executes all the steps in the workflow in sequence.
+// If a step's condition is not met, it is skipped.
+// If a step encounters an error, the onCancel callback is called and the error is returned.
+func (w *WorkflowBuilder) Run() error {
 	v := reflect.ValueOf(w.result).Elem()
-	for i, step := range w.steps {
+
+	for _, step := range w.steps {
+		if step.Condition != nil && !step.Condition() {
+			continue
+		}
+
 		stepResult, stepErr := step.Prompt()
 		if stepErr != nil {
+			if w.onCancel != nil {
+				w.onCancel(step.Name, stepErr)
+			}
 			return stepErr
+		}
+		if !step.SetResult {
+			continue
+		}
+
+		field := v.FieldByName(capitalize(step.Name))
+		if !field.IsValid() {
+			panic(fmt.Sprintf("workflow error: invalid field `%s`", step.Name))
+		}
+		if !field.CanSet() {
+			panic(fmt.Sprintf("workflow error: cannot set field `%s`", step.Name))
 		}
 
 		stepResultVal := reflect.ValueOf(stepResult)
-		if field := v.FieldByName(step.Name); field.IsValid() && field.CanSet() && stepResultVal.Type().AssignableTo(field.Type()) {
-			field.Set(stepResultVal)
-		} else if field := v.FieldByName(capitalize(step.Name)); field.IsValid() && field.CanSet() && stepResultVal.Type().AssignableTo(field.Type()) {
-			field.Set(stepResultVal)
-		} else {
-			panic(fmt.Sprintf("workflow error: cannot set field `%s` from step `%d`", step.Name, i))
+		if !stepResultVal.Type().AssignableTo(field.Type()) {
+			panic(fmt.Sprintf("workflow error: type `%s` is not assignable to field `%s`", stepResultVal.Type(), step.Name))
 		}
+
+		field.Set(stepResultVal)
 	}
 	return nil
 }
 
-func Workflow(v any) *workflowBuilder {
-	return &workflowBuilder{result: v}
+func Workflow(v any) *WorkflowBuilder {
+	return &WorkflowBuilder{result: v}
 }
 
 func capitalize(str string) string {
 	if str == "" {
 		return str
 	}
-	return strings.ToUpper(string(str[0])) + strings.ToLower(str[1:])
+	return strings.ToUpper(string(str[0])) + (str[1:])
 }
--- prompts/workflow_test.go
+++ prompts/workflow_test.go
@@ -8,13 +8,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type WorkflowResult struct {
-	Name string
-	Age  int
-}
-
 func TestWorkflowStep(t *testing.T) {
-	var r WorkflowResult
+	var r struct {
+		Name string
+	}
 
 	prompts.Workflow(&r).
 		Step("Name", func() (any, error) {
@@ -25,8 +22,90 @@ func TestWorkflowStep(t *testing.T) {
 	assert.Equal(t, "test name", r.Name)
 }
 
+func TestWorkflowConditionalStep(t *testing.T) {
+	var r struct {
+		Project string
+		Tools   []string
+	}
+	var project string
+	w := prompts.Workflow(&r).
+		Step("Project", func() (any, error) {
+			return project, nil
+		}).
+		ConditionalStep("Tools",
+			func() bool { return r.Project == "Node" },
+			func() (any, error) { return []string{"Eslint"}, nil },
+		)
+
+	project = "Go"
+	w.Run()
+	assert.Equal(t, []string(nil), r.Tools)
+
+	project = "Node"
+	w.Run()
+	assert.Equal(t, []string{"Eslint"}, r.Tools)
+}
+
+func TestWorkflowForkStep(t *testing.T) {
+	type PackageManager struct {
+		Command string
+		Args    []string
+	}
+	var r struct {
+		Install        bool
+		PackageManager PackageManager
+	}
+	var install bool
+	w := prompts.Workflow(&r).
+		Step("Install", func() (any, error) {
+			return install, nil
+		}).
+		ForkStep("PackageManager",
+			func() bool { return r.Install },
+			func() *prompts.WorkflowBuilder {
+				return prompts.Workflow(&r.PackageManager).
+					Step("Command", func() (any, error) {
+						return "npm", nil
+					}).
+					Step("Args", func() (any, error) {
+						return []string{"install"}, nil
+					})
+			},
+		)
+
+	install = true
+	w.Run()
+	assert.Equal(t, true, r.Install)
+	assert.Equal(t, "npm", r.PackageManager.Command)
+	assert.Equal(t, []string{"install"}, r.PackageManager.Args)
+
+	install = false
+	r.PackageManager = PackageManager{}
+	w.Run()
+	assert.Equal(t, false, r.Install)
+	assert.Equal(t, "", r.PackageManager.Command)
+	assert.Equal(t, []string(nil), r.PackageManager.Args)
+}
+
+func TestWorkflowLogStep(t *testing.T) {
+	var r struct{}
+	var calledTimes int
+
+	err := prompts.Workflow(&r).
+		LogStep("Name", func() {
+			calledTimes++
+		}).
+		Run()
+
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 1, calledTimes)
+}
+
 func TestWorkflowStepError(t *testing.T) {
-	var r WorkflowResult
+	var r struct {
+		Name string
+		Age  int
+	}
 	var calledTimes int
 
 	err := prompts.Workflow(&r).
@@ -35,7 +114,7 @@ func TestWorkflowStepError(t *testing.T) {
 			return "", errors.New("test error")
 		}).
 		Step("Age", func() (any, error) {
-			calledTimes++
+			assert.FailNow(t, "Age step should not be called")
 			return 22, nil
 		}).
 		Run()
@@ -44,8 +123,38 @@ func TestWorkflowStepError(t *testing.T) {
 	assert.Equal(t, 1, calledTimes)
 }
 
+func TestWorkflowOnCancel(t *testing.T) {
+	var r struct {
+		Name string
+		Age  int
+	}
+	var calledTimes int
+
+	err := prompts.Workflow(&r).
+		Step("Name", func() (any, error) {
+			calledTimes++
+			return "", errors.New("test error")
+		}).
+		Step("Age", func() (any, error) {
+			assert.FailNow(t, "Age step should not be called")
+			return 22, nil
+		}).
+		OnCancel(func(step string, err error) {
+			calledTimes++
+			assert.Equal(t, "test error", err.Error())
+			assert.Equal(t, "Name", step)
+		}).
+		Run()
+
+	assert.Equal(t, "test error", err.Error())
+	assert.Equal(t, 2, calledTimes)
+}
+
 func TestWorkflowCaseInsensitive(t *testing.T) {
-	var r WorkflowResult
+	var r struct {
+		Name string
+		Age  int
+	}
 
 	prompts.Workflow(&r).
 		Step("name", func() (any, error) {
